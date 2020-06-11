d14fe6d68d49f5343cd74e608d4ed88ec005d8b3
Nick Pang <nick95927@gmail.com>
2020-06-12 03:21:03 +1000

    implemented catch time, average pt per hour & average round per hour

--- src/component/RankingTop10.js
+++ src/component/RankingTop10.js
@@ -11,6 +11,8 @@ export default function RankingTop10({eventStartTimestamp, eventDuration, roundM
 	const [gridApi, setGridApi] = useState(null);
 
 	// var gridData = [];
+	var formattedTimestamp = timestampToDateTime(rawData.lastModified);
+	var eventProgressed = getTimeDifference(rawData.lastModified, eventStartTimestamp);
 
 	const [columnDefs, setColumnDefs] = useState([
 			{ headerName: "Top", field: "rank", sortable: true, filter: "agNumberColumnFilter", maxWidth: 80 },
@@ -21,7 +23,7 @@ export default function RankingTop10({eventStartTimestamp, eventDuration, roundM
 			{ headerName: "前位差", field: "diff_last", sortable: true, filter: false, valueFormatter: numberFormatter, maxWidth: 120 },
 			{ headerName: "前位回数差", field: "diff_last_round", sortable: true, filter: false, valueFormatter: numberFormatter, maxWidth: 150 },
 			{ headerName: "追撃時間", field: "catch_time", sortable: true, filter: false, maxWidth: 150 },
-			{ headerName: "pt時速", field: "point_per_hour", sortable: true, filter: false, maxWidth: 100 },
+			{ headerName: "pt時速", field: "point_per_hour", sortable: true, filter: false, valueFormatter: numberFormatter, maxWidth: 100 },
 			{ headerName: "平均周回時速", field: "round_per_hour", sortable: true, filter: false, maxWidth: 150 },
 			{ headerName: "pt/10分", field: "point_10mins", sortable: true, filter: false, valueFormatter: numberFormatter, maxWidth: 100 },
 			{ headerName: "pt/30分", field: "point_30mins", sortable: true, filter: false, valueFormatter: numberFormatter, maxWidth: 100 },
@@ -52,13 +54,13 @@ export default function RankingTop10({eventStartTimestamp, eventDuration, roundM
 					userId: top10[i].userId,
 					point: top10[i].point,
 					userDeck: top10[i].userDeck,
-					bonus: 0,
+					bonus: 0, // need to implement
 					diff_1st: (i > 0 ? (top10[i].point - top10[0].point) : 0),
 					diff_last: (i > 0 ? (top10[i].point - top10[i - 1].point) : 0),
-					diff_last_round: 0, // Need to change
-					catch_time: 0, // need to change
-					point_per_hour: 0, // need to change
-					round_per_hour: 0, // need to change
+					diff_last_round: (i > 0 ? Math.ceil((top10[i - 1].point - top10[i].point) / roundMaxPt)*-1 : 0),
+					catch_time: secondsToHrsAndMins(i > 0 ? Math.ceil((top10[i - 1].point - top10[i].point) / roundMaxPt * fastestRound) : 0),
+					point_per_hour: (top10[i].point / (eventProgressed / 3600)),
+					round_per_hour: (top10[i].point / (eventProgressed / 3600) / roundMaxPt).toFixed(3), // need to change
 					point_10mins: 0, // need to change
 					point_30mins: 0, // need to change
 					point_60mins: 0, // need to change
@@ -68,47 +70,6 @@ export default function RankingTop10({eventStartTimestamp, eventDuration, roundM
 			}
 		}
 		return result;
-	}
-
-	async function getAllData() {
-		let response = await fetch("https://cronpublic.yasushi.me/ranking.json");
-		let data = await response.json();
-		console.log(data);
-		return data;
-	}
-
-	function updateAllData() {
-		// This is an async function.
-		getAllData()
-		.then(res => {
-			return {
-					lastModified: res.lastModified,
-					topUsers: res.topUsers
-			};
-		})
-		.then(ranks => setRawData(ranks));
-	}
-
-  useEffect(() => {
-		// Call for first time without delay.
-		updateAllData();
-		const interval = setInterval(() => {
-			// Loop from second request.
-			updateAllData();
-		}, 59000);
-		return () => clearInterval(interval);
-	}, []);
-
-  function onGridReady(params) {
-		setGridApi(params.api);
-		// params.api.sizeColumnsToFit();
-
-		// var columnAPI = params.columnApi;
-		// var allColumnIds = [];
-    // columnAPI.getAllColumns().forEach(function(column) {
-    //   allColumnIds.push(column.colId);
-    // });
-    // columnAPI.autoSizeColumns(allColumnIds, false);
 	}
 
 	function timestampToDateTime(timestamp) {
@@ -140,30 +101,73 @@ export default function RankingTop10({eventStartTimestamp, eventDuration, roundM
 		return (hours+" 小時 "+mins+" 分鐘");
 	}
 
-	var formattedTimestamp = timestampToDateTime(rawData.lastModified);
-	var eventProgressed = getTimeDifference(rawData.lastModified, eventStartTimestamp);
+  function onGridReady(params) {
+		setGridApi(params.api);
+		// params.api.sizeColumnsToFit();
+
+		// var columnAPI = params.columnApi;
+		// var allColumnIds = [];
+    // columnAPI.getAllColumns().forEach(function(column) {
+    //   allColumnIds.push(column.colId);
+    // });
+    // columnAPI.autoSizeColumns(allColumnIds, false);
+	}
+
+	async function getAllData() {
+		let response = await fetch("https://cronpublic.yasushi.me/ranking.json");
+		let data = await response.json();
+		console.log(data);
+		return data;
+	}
+
+	function updateAllData() {
+		// This is an async function.
+		getAllData()
+		.then(res => {
+			return {
+					lastModified: res.lastModified,
+					topUsers: res.topUsers
+			};
+		})
+		.then(ranks => setRawData(ranks));
+	}
+	
+  useEffect(() => {
+		// Call for first time without delay.
+		updateAllData();
+		const interval = setInterval(() => {
+			// Loop from second request.
+			updateAllData();
+		}, 59000);
+		return () => clearInterval(interval);
+	}, []);
 
 	return (
 		<div>
 			<div>
-				<h4>最終更新日時: {formattedTimestamp}</h4>
+				{/* <h4>最終更新日時: {formattedTimestamp}</h4> */}
 				{/* <h5>dump: {eventStartTimestamp}, {eventDuration}, {roundMaxPt}, {fastestRound} </h5> */}
 				<h5>活動進度: {secondsToHrsAndMins(eventProgressed)}</h5>
 				<h5>活動剩餘: {secondsToHrsAndMins((eventDuration * 60 * 60) - eventProgressed)}</h5>
 			</div>
+			<div>
+				<ProgressBar animated now={95} />
+			</div>
 			<div class="container">
-			<ProgressBar animated now={45} />
 				<div id="myGrid" className="ag-theme-alpine-dark" 
-				style={{ height: "540px", width: "95%" }}>
+				style={{ height: "500px", width: "95%" }}>
 					<AgGridReact
 						columnDefs={columnDefs}
 						rowData={getTop10Data(rawData)}
 						onGridReady={onGridReady}
 						// onRowClicked={}
-						pagination={true}
-						paginationPageSize={10}
+						// pagination={true}
+						// paginationPageSize={10}
 					/>
 				</div>
+			</div>
+			<div>
+				<h5>最終更新日時: {formattedTimestamp}</h5>
 			</div>
 		</div>
 	);
